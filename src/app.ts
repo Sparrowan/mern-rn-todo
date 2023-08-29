3d821c7b81316d7fd6b4de14b4b28630f77379b9
Alphius <alphiuswambua@gmail.com>
2023-08-29 20:56:11 +0300

    added all todos cruds

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-console.log("server is running")
+import express, { Express } from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import todoRoutes from "./routes"
+
+const app: Express = express()
+
+const PORT: string | number = process.env.PORT || 4000
+
+app.use(cors())
+app.use(todoRoutes)
+
+const uri: string = process.env.URI!
+
+mongoose
+    .connect(uri)
+    .then(() =>
+        app.listen(PORT, () =>
+            console.log(`Server running on http://localhost:${PORT}`)
+        )
+    )
+    .catch(error => {
+        console.log(error)
+        throw error
+    })
